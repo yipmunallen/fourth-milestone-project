419172adb62d8c381ae07b030bbfc069ee9bed5a
yipmunallen <ymallen@hotmail.co.uk>
2021-06-12 19:35:00 +0000

    Added category filtering in "views.py"

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 
 def all_products(request):
@@ -9,23 +9,32 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    current_category = None
 
     if request.GET:
+
+        if 'category' in request.GET:
+            category = request.GET['category']
+            products = products.filter(category__name=category)
+            current_category = Category.objects.filter(name=category)
+
         if 'search' in request.GET:
             query = request.GET['search']
             if not query:
                 messages.error(
-                    request, "You didn't enter any search criteria!")
+                    request, "Please enter a search criteria")
                 return redirect(reverse('products'))
 
-            queries = Q(name__icontains=query) | Q( 
-                description__icontains=query)
-
-            products = products.filter(queries) 
+            queries = Q(
+                name__icontains=query) | Q(
+                description__icontains=query)| Q(
+                category__name__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
         'search_term': query,
+        'current_category': current_category,
     }
 
     return render(request, 'products/products.html', context)
