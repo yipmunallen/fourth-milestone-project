2eec73837fcaad72f822621d646ea2b8e200d2d8
yipmunallen <ymallen@hotmail.co.uk>
2021-06-14 10:54:16 +0000

    Access session cart in "contexts.py"

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def cart_contents(request):
@@ -7,6 +9,17 @@ def cart_contents(request):
     cart_items = []
     subtotal = 0
     product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        subtotal += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if subtotal < settings.FREE_DELIVERY_THRESHOLD:
         delivery = subtotal * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
