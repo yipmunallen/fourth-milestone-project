84722122e179e58cad8b02541dd2c9dbb4441acc
yipmunallen <ymallen@hotmail.co.uk>
2021-06-22 17:44:03 +0000

    Added default from email field

--- neat/settings.py
+++ neat/settings.py
@@ -178,6 +178,9 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+
+DEFAULT_FROM_EMAIL = 'neat@example.com'
 
 
 # Default primary key field type
