67e6a18478eeda9b9fdc3cff82c4f0f9a9395849
yipmunallen <ymallen@hotmail.co.uk>
2021-06-25 21:50:23 +0000

    Created post view in blog "views.py"

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Post
 
-# Create your views here.
+
+def post(request):
+    """ A view to return the blog page """
+
+    posts = Post.objects.filter(status=1)
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'blog/blog.html', context)
