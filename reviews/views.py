7cd3f94ebdb6edba7417cc9655385da6a2de4645
yipmunallen <ymallen@hotmail.co.uk>
2021-06-25 16:53:17 +0000

    Created edit_product view in reviews "views.py"

--- reviews/views.py
+++ reviews/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
+from .models import Review
 from .forms import ReviewForm
 from products.models import Product
 from profiles.models import UserProfile
@@ -38,4 +39,23 @@ def add_review(request, product_id):
             request,
             'Failed to add review. Please ensure the form is valid.')
 
-    return redirect(reverse('product_item', args=(product_id,)))
+    return redirect(reverse('product_detail', args=(product_id,)))
+
+
+@login_required
+def edit_review(request, review_id):
+    """ Allows users to edit their review """
+
+    review = get_object_or_404(Review, pk=review_id)
+    review_form = ReviewForm(request.POST, request.FILES, instance=review)
+
+    if review_form.is_valid():
+        review.save()
+
+        messages.success(request, 'Your review has been successfully edited')
+    else:
+        messages.error(
+            request,
+            'Failed to edit review. Please ensure the form is valid.')
+
+    return redirect(reverse('product_detail', args=(review.product.id,)))
