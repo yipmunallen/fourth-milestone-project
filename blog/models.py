4b28d18282e7ceffce35de2520e8b5b09b1a7a28
yipmunallen <ymallen@hotmail.co.uk>
2021-06-25 20:13:27 +0000

    Removed updated on and changed order of fields in blog "models.py"

---
+++ blog/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+from profiles.models import UserProfile
+
+
+class Post(models.Model):
+    """
+    A blog model that allows admin users to create Blog posts
+    """
+
+    STATUS = (
+        (0, "Draft"),
+        (1, "Publish")
+    )
+
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.CharField(max_length=60)
+    summary = models.TextField(max_length=300)
+    content = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    """
+    A blog model that allows users to comment on blog posts
+    """
+
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    comment = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['date_added']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.comment, self.user)
