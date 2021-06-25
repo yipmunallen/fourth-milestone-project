0765c080cfd4aad2553d499a3a8e780582a58874
yipmunallen <ymallen@hotmail.co.uk>
2021-06-25 14:29:25 +0000

    Changed would recommend field so required in reviews "models.py"

--- reviews/models.py
+++ reviews/models.py
@@ -23,7 +23,7 @@ class Review(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     rating = models.IntegerField(choices=RATING)
-    would_recommend = models.BooleanField(default=False, null=True, blank=True)
+    would_recommend = models.BooleanField(default=False, null=False, blank=False)
     date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
