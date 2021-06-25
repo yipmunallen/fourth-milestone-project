1f409bc8ef6b6b788df3a962127d87647a02341d
yipmunallen <ymallen@hotmail.co.uk>
2021-06-25 10:49:35 +0000

    Updated admin for reviews model in "admin.py"

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'title',
+        'product',
+        'user',
+        'rating',
+        'would_recommend',
+        'date_created'
+    )
+
+
+admin.site.register(Review, ReviewAdmin)
