6e92678958d42a66b86682bfd5c94faed4ce2354
yipmunallen <ymallen@hotmail.co.uk>
2021-06-26 11:04:03 +0000

    Add add_post path to blog "urls.py"

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.all_posts, name='posts'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('add_post', views.add_post, name='add_post'),
 ]
