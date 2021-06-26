001cb02a74701b600834af3d1882125d74409c40
yipmunallen <ymallen@hotmail.co.uk>
2021-06-26 11:30:40 +0000

    Created edit post path in blog "urls.py"

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.all_posts, name='posts'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
     path('add_post', views.add_post, name='add_post'),
+    path('edit_post/<slug:slug>/', views.edit_post, name='edit_post'),
 ]
