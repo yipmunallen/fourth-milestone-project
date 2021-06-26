ac36b6bbe1eafcb44efd2173fa1f3d2565cc526d
yipmunallen <ymallen@hotmail.co.uk>
2021-06-26 14:20:12 +0000

    Created comment form in blog "forms.py"

--- blog/forms.py
+++ blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from products.widgets import CustomClearableFileInput
 
 from .models import Post
+from .models import Comment
 
 
 class PostForm(forms.ModelForm):
@@ -18,3 +19,15 @@ class PostForm(forms.ModelForm):
   
         self.fields['title'].widget.attrs['autofocus'] = True
 
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = [
+            'comment'
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['comment'].widget.attrs['placeholder'] = 'Write your comment here...'
